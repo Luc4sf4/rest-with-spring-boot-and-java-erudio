691de35c767e989968f32c0def298c4922200e9a
Lucasf4 <superlucas2005@gmail.com>
2023-08-14 14:46:37 -0300

    changing commits

--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Greeting.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Greeting.java
@@ -1,16 +1,13 @@
 package br.com.erudio;
 
 public class Greeting {
-	
+
 	private final long id;
-	
 	private final String content;
 	
-	public Greeting (long id, String content) {
-		
-		this.id= id;
-		this.content =content;
-		
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
 	}
 
 	public long getId() {
@@ -20,8 +17,4 @@ public class Greeting {
 	public String getContent() {
 		return content;
 	}
-	
-	
-	
-	
 }
