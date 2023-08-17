9a8d6bdc892d0f55695ae2b7e493eeeba4131d4d
Lucasf4 <superlucas2005@gmail.com>
2023-08-17 11:57:34 -0300

    Using Standarts of VO projects

--- 08_implementingTheAPI_IN_MySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
+++ 08_implementingTheAPI_IN_MySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,4 +1,4 @@
-package br.com.erudio;
+package br.com.erudio.services;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -10,70 +10,62 @@ import br.com.erudio.exception.ResourceNotFoundException;
 import br.com.erudio.model.Person;
 import br.com.erudio.repositorys.PersonRepository;
 
-@Service //Using to Spring Boot see this class wil be inject in runtime in another class in this application
+@Service // Using to Spring Boot see this class wil be inject in runtime in another class
+			// in this application
 public class PersonServices {
 
-	
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
 	@Autowired
-	PersonRepository repository;
-	
-	
+	private PersonRepository repository;
+
 	public Person create(Person person) {
-		
+
 		logger.info("Creating one person! ");
-		
+
 		return repository.save(person);
-		
+
 	}
-	
-public Person update(Person person) {
-		
+
+	public Person update(Person person) {
+
 		logger.info("Updating one person! ");
-		
-		
+
 		Person entity = repository.findById(person.getId())
 				.orElseThrow(() -> new ResourceNotFoundException("Not found for this id"));
-		
-		
+
 		entity.setFirstName(person.getFirstName());
 		entity.setLastName(person.getLastName());
 		entity.setAddress(person.getAddress());
 		entity.setGender(person.getGender());
-		
-		
+
 		return repository.save(person);
-		
+
 	}
 
-public void delete(Long id) {
-	
-	logger.info("deleting one person! ");
-	
-	Person entity = repository.findById(id)
-			.orElseThrow(() -> new ResourceNotFoundException("Not found for this id"));
-	
+	public void delete(Long id) {
+
+		logger.info("deleting one person! ");
+
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Not found for this id"));
+
 		repository.delete(entity);
-}
-	
-	
+	}
+
 	public List<Person> findAll() {
-		
+
 		logger.info("Find all people! ");
 
 		return repository.findAll();
-		
+
 	}
 
 	public Person findByID(Long id) {
-		
+
 		logger.info("Finding one Person");
-		
-		
-		
-		
+
 		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Not found for this id"));
 	}
-	
+
 }
--- 09_StandartsOfVOProjects/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVo.java
+++ 09_StandartsOfVOProjects/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVo.java
@@ -1,18 +1,9 @@
-package br.com.erudio.model;
+package br.com.erudio.data.vo.v1;
 
 import java.io.Serializable;
 import java.util.Objects;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name= "person")
-public class Person implements Serializable {
+public class PersonVo implements Serializable {
 
 	/**
 	 * 
@@ -23,24 +14,22 @@ public class Person implements Serializable {
 	/**/
 	
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY	 )
 	private Long id;
 	
 	
-	@Column (name ="first_name", nullable = false, length = 80)
+	
 	private String firstName;
 	
-	@Column (name ="last_name", nullable = false, length = 80)
+
 	private String lastName;
 	
-	@Column(nullable = false,length = 255)
+	
 	private String address;
 	
-	@Column(nullable = false,length = 6)
+	
 	private String gender ;
 	
-	public Person() {}
+	public PersonVo() {}
 
 	public Long getId() {
 		return id;
@@ -95,7 +84,7 @@ public class Person implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Person other = (Person) obj;
+		PersonVo other = (PersonVo) obj;
 		return Objects.equals(address, other.address) && Objects.equals(firstName, other.firstName)
 				&& Objects.equals(gender, other.gender) && id == other.id && Objects.equals(lastName, other.lastName);
 	}
