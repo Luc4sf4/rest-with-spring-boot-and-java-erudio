02dd695e282973913a74f576a2491472ccd80a0f
Lucasf4 <superlucas2005@gmail.com>
2023-08-28 15:56:38 -0300

    Content Configuration

---
+++ 13_ContenNegotiation/rest-with-spring-boot-and-java-erudio/target/classes/db/migration/V1__Create_Table_person.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `person` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `address` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
